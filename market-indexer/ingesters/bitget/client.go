bea6fde1f7e96b3e6cf399550b2d428cd8163467
SeUkKim <eleccookie@gmail.com>
2025-04-25 01:07:47 +0900

    wip: add bitget

---
+++ market-indexer/ingesters/bitget/client.go
@@ -0,0 +1,70 @@
+package bitget
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/skip-mev/connect-mmu/lib/http"
+	"go.uber.org/zap"
+)
+
+const (
+	EndpointSymbols = "https://api.bitget.com/api/v2/spot/public/symbols"
+
+	EndpointTickers = "https://api.bitget.com/api/v2/spot/market/tickers"
+)
+
+var _ Client = &httpClient{}
+
+type Client interface {
+	// Symbols gets all symbols from Bitget
+	Symbols(ctx context.Context) (BitgetSymbolsResponse, error)
+
+	// Tickers gets all tickers from Bitget
+	Tickers(ctx context.Context) (BitgetTickersResponse, error)
+}
+
+type httpClient struct {
+	client *http.Client
+	logger *zap.Logger
+}
+
+func NewHttpClient(logger *zap.Logger) Client {
+	return &httpClient{
+		client: http.NewClient(),
+		logger: logger,
+	}
+}
+
+func (h *httpClient) Symbols(ctx context.Context) (BitgetSymbolsResponse, error) {
+	resp, err := h.client.GetWithContext(ctx, EndpointSymbols)
+	if err != nil {
+		h.logger.Error("failed to get symbols", zap.Error(err))
+		return BitgetSymbolsResponse{}, err
+	}
+	defer resp.Body.Close()
+
+	var getInstrResp BitgetSymbolsResponse
+	if err := json.NewDecoder(resp.Body).Decode(&getInstrResp); err != nil {
+		h.logger.Error("failed to decode symbols", zap.Error(err))
+		return BitgetSymbolsResponse{}, err
+	}
+
+	return getInstrResp, nil
+}
+
+func (h *httpClient) Tickers(ctx context.Context) (BitgetTickersResponse, error) {
+	resp, err := h.client.GetWithContext(ctx, EndpointTickers)
+	if err != nil {
+		h.logger.Error("failed to get tickers", zap.Error(err))
+		return BitgetTickersResponse{}, err
+	}
+	defer resp.Body.Close()
+
+	var tickers BitgetTickersResponse
+	if err := json.NewDecoder(resp.Body).Decode(&tickers); err != nil {
+		h.logger.Error("failed to decode tickers", zap.Error(err))
+		return BitgetTickersResponse{}, err
+	}
+	
+	return tickers, nil
+}
---
+++ market-indexer/ingesters/bitget/ingester.go
@@ -0,0 +1,90 @@
+package bitget
+
+import (
+	"context"
+	"fmt"
+
+	"go.uber.org/zap"
+
+	"github.com/skip-mev/connect-mmu/market-indexer/ingesters"
+	"github.com/skip-mev/connect-mmu/market-indexer/ingesters/types"
+	"github.com/skip-mev/connect-mmu/store/provider"
+)
+
+const (
+	Name         = "bitget"
+	ProviderName = Name + types.ProviderNameSuffixWS
+
+	StatusOnline = "online"
+)
+
+var _ ingesters.Ingester = &Ingester{}
+
+type Ingester struct {
+	logger *zap.Logger
+	client Client
+}
+
+func (i *Ingester) Name() string {
+	return Name
+}
+
+func New(logger *zap.Logger) *Ingester {
+	if logger == nil {
+		panic("cannot set nil logger")
+	}
+
+	return &Ingester{
+		logger: logger.With(zap.String("ingester", Name)),
+		client: NewHttpClient(logger),
+	}
+}
+
+func NewWithClient(logger *zap.Logger, client Client) *Ingester {
+	if logger == nil {
+		panic("cannot set nil logger")
+	}
+
+	return &Ingester{
+		logger: logger.With(zap.String("ingester", Name)),
+		client: client,
+	}
+}
+
+func (i *Ingester) GetProviderMarkets(ctx context.Context) ([]provider.CreateProviderMarket, error) {
+	i.logger.Info("fetching data")
+
+	symbols, err := i.client.Symbols(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	tickers, err := i.client.Tickers(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	tickerMap := tickers.toMap()
+
+	pms := make([]provider.CreateProviderMarket, 0, len(symbols.SymbolData))
+	for _, item := range symbols.SymbolData {
+		if item.Status != StatusOnline {
+			continue
+		}
+
+		ticker, found := tickerMap[item.Symbol]
+		if !found {
+			return nil, fmt.Errorf("ticker not found for symbol %s", item.Symbol)
+		}
+
+		i.logger.Debug("ticker", zap.Any("data", ticker))
+
+		pm, err := item.toProviderMarket(ticker)
+		if err != nil {
+			return nil, err
+		}
+		pms = append(pms, pm)
+	}
+
+	return pms, nil
+}
